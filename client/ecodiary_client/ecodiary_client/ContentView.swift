2d0390d6184972236d8c06ec4af4ddf707151fec
k2645 <k2645@naver.com>
2021-08-21 15:16:38 +0900

    프로젝트 생성

---
+++ client/ecodiary_client/ecodiary_client/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ecodiary_client
+//
+//  Created by 김영현 on 2021/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ client/ecodiary_client/ecodiary_client/ecodiary_clientApp.swift
@@ -0,0 +1,17 @@
+//
+//  ecodiary_clientApp.swift
+//  ecodiary_client
+//
+//  Created by 김영현 on 2021/08/21.
+//
+
+import SwiftUI
+
+@main
+struct ecodiary_clientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
