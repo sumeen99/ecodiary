a5c308b77a4b4d1182d7531af3230c15db614686
sumeen99 <70104259+sumeen99@users.noreply.github.com>
2021-09-07 04:57:41 +0900

    Test짜다가 우선 User부분에서 포기... 내가보기엔 테이블 만들고 시작해야할듯.. 아 근데 생각해보니 테이블 내가 못만들게 해놨구나...?아하! 멍청쓰멍청쓰~ 우선 그러면 내일 만들고! service test랑 controller 하자! controller만 끝나면 우선 일단락인데ㅠ

--- server/server/src/test/java/ecodiary/server/domain/Posts/PostsRepositoryTest.java
+++ server/server/src/test/java/ecodiary/server/domain/Posts/PostsRepositoryTest.java
@@ -3,26 +3,54 @@ package ecodiary.server.domain.Posts;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.in;
 
 @SpringBootTest
+@Transactional
 public class PostsRepositoryTest {
 
     @Autowired
     PostsRepository postsRepository;
 
     @Test
-    public void all(){
-        List<Posts> postsList = postsRepository.findAll();
+    public void findAll(){
+
+        //given
         Long id=1L;
         String title="오늘 하루 일회용품 대신 텀블러 써보기";
 
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
         Posts posts = postsList.get(0);
         assertThat(posts.getId()).isEqualTo(id);
         assertThat(posts.getMission()).isEqualTo(title);
 
     }
+
+    @Test
+    public void save(){
+        //given
+        String mission="오늘의 미션";
+        String question="오늘의 질문";
+        String info="오늘의 정보";
+
+        //when
+        Posts post=postsRepository.save(Posts.builder().mission(mission).question(question).info(info).build());
+
+        //then
+        Posts posts=postsRepository.findById(post.getId()).get();
+        assertThat(post).isEqualTo(posts);
+
+    }
+
+
+
+
 }
--- server/server/src/test/java/ecodiary/server/domain/User/UserRepositoryTest.java
+++ server/server/src/test/java/ecodiary/server/domain/User/UserRepositoryTest.java
@@ -1,13 +1,30 @@
 package ecodiary.server.domain.User;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import static org.assertj.core.api.Assertions.assertThat;
 
+@Transactional
 @SpringBootTest
 public class UserRepositoryTest {
 
     @Autowired
     UserRepository userRepository;
 
+    @Test
+    public void save(){
+        //given
+        Long missionId=1L;
 
+        //when
+        User users=userRepository.save(User.builder().missionId(missionId).build());
+
+        //then
+        User user=userRepository.findById(users.getId()).get();
+        assertThat(user).isEqualTo(users);
+
+
+    }
 }
