e4363cb4123844a8021e6fe99bff54ce59c4ad2d
sumeen99 <70104259+sumeen99@users.noreply.github.com>
2021-09-18 05:31:40 +0900

    서버시간비교업데이트

--- server/server/src/main/java/ecodiary/server/domain/Posts/PostsController.java
+++ server/server/src/main/java/ecodiary/server/domain/Posts/PostsController.java
@@ -1,9 +1,12 @@
 package ecodiary.server.domain.Posts;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,4 +28,8 @@ public class PostsController {
     public String findInfo(@PathVariable Long id){
         return postsService.findInfo(id);
     }
+
+    @GetMapping("/api/v1/{date}")
+    public Boolean findDate(@PathVariable @DateTimeFormat(pattern="yyyyMMdd") LocalDate date){
+        return date.equals(LocalDate.now()); }
 }
